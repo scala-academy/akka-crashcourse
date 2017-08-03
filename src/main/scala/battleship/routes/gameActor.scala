bb41fa75d1ffc4c6be0bce1ad66526141c940366
Joris Luijsterburg <joris.luijsterburg@ing.nl>
2017-08-03 15:55:35 +0200

    Forgot to git add

---
+++ src/main/scala/battleship/routes/gameActor.scala
@@ -0,0 +1,79 @@
+package battleship.routes
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import battleship.PlayerActor.{GetNextShot, PlaceBoats}
+import battleship.game._
+import battleship.routes.gameActor.{StartGame, ThisIsBoatSetup, ThisIsNextMove}
+
+/**
+  * Created by m06f947 on 3-8-2017.
+  */
+
+/////////////////Nothing tested yet//////////////////////////////////
+
+
+object gameActor {
+
+  case class StartGame(boardsize:Int, player1:ActorRef, player2:ActorRef)
+
+  //case class WhatIsBoatSetup(boats:Seq[Boat], boardsize: Int)
+  case class ThisIsBoatSetup(placement: Set[(Boat,BoatLocation)])
+  //case class WhatIsNextMove(boardsize:Int, shotHistory: Seq[((Int,Int),ShotResult)])
+  case class ThisIsNextMove(x: Int, y: Int)
+
+
+  //declare winner
+
+
+}
+
+class gameActor extends Actor with ActorLogging {
+
+
+  //internal state
+  var boardsize = 8
+  //first of pair is currentplayer
+  var players: (ActorRef, ActorRef) = (Actor.noSender, Actor.noSender)
+  //Have the boatsetup ready. In here player1 is boatsetup._1
+  var boatsetup: Map[ActorRef, Set[(Boat, BoatLocation)]] = Map()
+  //keep the state of the boards
+  var boardstates: Map[ActorRef, BoardState] = Map()
+
+  def initPlayerState(playerToPlaceBoats: ActorRef): BoardState = {
+    val placements = boatsetup(ActorRef)
+    placements.foldLeft(BoardState.empty) {
+      case (boardState, (boat, location)) => boardState.placeBoat(boat, location)
+    }
+  }
+
+  override def receive: Receive = {
+    case StartGame(size, player1, player2) => {
+      players = (player1, player2)
+      boardsize = size
+      players._1 ! PlaceBoats(Game.defaultBoatSet, boardsize)
+    }
+    case ThisIsBoatSetup(placement) if sender() == players._1 => {
+      //Right now: If players send multiple placements, they will overwrite the other.
+      if (boatsetup.count((a: (ActorRef, Set[(Boat, BoatLocation)])) => true) < 2) {
+        boatsetup += (players._1 -> placement)
+        players = (players._2, players._1)
+        players._1 ! PlaceBoats(Game.defaultBoatSet, boardsize)
+      }
+      else {
+        boardstates += (players._1 -> initPlayerState(players._2))
+        boardstates += (players._2 -> initPlayerState(players._1))
+        players._1 ! GetNextShot(boardsize, List())
+      }
+    }
+    case ThisIsNextMove(x, y) if sender() == players._1 => {
+      boardstates(players._1).processShot((x, y))
+      if (boardstates(players._1).allShipsSunk) log.info("Player 1 has won!!")
+      ///Exit???
+      else {
+        players = (players._2, players._1)
+        players._1 ! GetNextShot(boardsize, List())
+      }
+    }
+  }
+}
+
