b6b17a9ff64904543806a00ecefbdb7bd97fa35f
Joris Luijsterburg <joris.luijsterburg@ing.nl>
2017-08-09 08:29:48 +0200

    added unittests and some code formatting

--- src/main/scala/battleship/GameActor.scala
+++ src/main/scala/battleship/GameActor.scala
@@ -10,12 +10,11 @@ import battleship.game._
   */
 
 
-
 object GameActor {
 
-  case class StartGame(boardsize:Int, player1:ActorRef, player2:ActorRef, boatset: Seq[Boat])
+  case class StartGame(boardsize: Int, player1: ActorRef, player2: ActorRef, boatset: Seq[Boat])
 
-  case class ThisIsBoatSetup(placement: Set[(Boat,BoatLocation)])
+  case class ThisIsBoatSetup(placement: Set[(Boat, BoatLocation)])
 
   case class ThisIsNextMove(x: Int, y: Int)
 
@@ -36,129 +35,56 @@ class GameActor extends Actor with ActorLogging {
     }
   }
 
-  def swapCurrentPlayer(one:ActorRef,two:ActorRef,current:ActorRef) = {
+  def swapCurrentPlayer(one: ActorRef, two: ActorRef, current: ActorRef) = {
     if (one == current) two
     else one
   }
 
 
-
-
-override def receive: Receive = {
-  case StartGame(size, player1, player2, boatset) => {
-    player1 ! PlaceBoats(boatset, size)
-    sender() ! "Game started!"
-    context.become(gameinit1(size, player1, player2, boatset))
+  override def receive: Receive = {
+    case StartGame(size, player1, player2, boatset) => {
+      player1 ! PlaceBoats(boatset, size)
+      sender() ! "Game started!"
+      context.become(gameinit1(size, player1, player2, boatset))
+    }
+    case WhatIsGameState => sender() ! GameNotStartedYet(0)
   }
-  case WhatIsGameState => sender() ! GameNotStartedYet(0)
-}
 
-  def gameinit1(size:Int,player1:ActorRef,player2:ActorRef,boatset: Seq[Boat]): Receive = {
-    case ThisIsBoatSetup(placement1) if (sender() == player1) =>
+  def gameinit1(size: Int, player1: ActorRef, player2: ActorRef, boatset: Seq[Boat]): Receive = {
+    case ThisIsBoatSetup(placement1) if sender() == player1 =>
       player2 ! PlaceBoats(boatset, size)
-      context.become(gameinit2(size, player1, player2, boatset,placement1))
+      context.become(gameinit2(sender(), size, player1, player2, placement1))
     case WhatIsGameState => sender() ! GameNotStartedYet(1)
   }
 
-  def gameinit2(size:Int,player1:ActorRef,player2:ActorRef,boatset1:Seq[Boat],placement1:Set[(Boat,BoatLocation)]): Receive = {
-    case ThisIsBoatSetup(placement2) if (sender() == player2) =>
-      val boardstates = Map(player1 -> placeBoats(placement2),player2 -> placeBoats(placement1))
+  def gameinit2(gamestarter: ActorRef, size: Int, player1: ActorRef, player2: ActorRef, placement1: Set[(Boat, BoatLocation)]): Receive = {
+    case ThisIsBoatSetup(placement2) if sender() == player2 =>
+      val boardstates = Map(player1 -> placeBoats(placement2), player2 -> placeBoats(placement1))
       player2 ! GetNextShot(size, boardstates(player2).history)
-      context.become(gamestarted(size,player1,player2,player2,boardstates))
+      context.become(gamestarted(gamestarter, size, player1, player2, player2, boardstates))
     case WhatIsGameState => sender() ! GameNotStartedYet(2)
   }
 
-  def gamestarted(size:Int,player1:ActorRef,player2:ActorRef,currentplayer:ActorRef,boardstates: Map[ActorRef, BoardState] ):Receive = {
+  def gamestarted(gamestarter: ActorRef,size: Int, player1: ActorRef, player2: ActorRef, currentplayer: ActorRef, boardstates: Map[ActorRef, BoardState]): Receive = {
     case ThisIsNextMove(x, y) if sender() == currentplayer =>
       val nextboardstates = boardstates + (currentplayer -> boardstates(currentplayer).processShot((x, y)))
       if (nextboardstates(currentplayer).allShipsSunk) {
-        currentplayer ! ("Player " + currentplayer +  " has won!!") //this is for test only
-        log.info("Player " + currentplayer +  " has won!!")
-        context.become(endgame(currentplayer,boardstates))
+        gamestarter ! "The game ended and " + currentplayer + " has won"
+        currentplayer ! ("Player " + currentplayer + " has won!!") //this is for test only
+        log.info("Player " + currentplayer + " has won!!")
+        context.become(endgame(currentplayer, boardstates))
       }
       else {
-        val currentPlayer2 = swapCurrentPlayer(player1,player2,currentplayer)
+        val currentPlayer2 = swapCurrentPlayer(player1, player2, currentplayer)
         currentPlayer2 ! GetNextShot(size, nextboardstates(currentPlayer2).history)
-        context.become(gamestarted(size,player1,player2,currentPlayer2,nextboardstates))
+        context.become(gamestarted(gamestarter, size, player1, player2, currentPlayer2, nextboardstates))
       }
     case WhatIsGameState => sender() ! boardstates
   }
-  def endgame(currentplayer: ActorRef, boardstates: Map[ActorRef, BoardState]):Receive = {
+
+  def endgame(currentplayer: ActorRef, boardstates: Map[ActorRef, BoardState]): Receive = {
     case WhatIsGameState => sender() ! boardstates
-    case _ => sender() ! "The game ended and " + currentplayer +  " has won"
+    case _ => sender() ! "The game ended and " + currentplayer + " has won"
   }
-
-
-
-
-/*
-old definition
-
-  //internal state
-  var boardsize = Game.defaultBoardSize
-  var boatSet = Game.defaultBoatSet
-  //first of pair is currentplayer
-  var players: (ActorRef, ActorRef) = (Actor.noSender, Actor.noSender)
-  def currentPlayer = players._1
-  def swapPlayers = {players = (players._2, players._1)}
-  //Have the boatsetup ready. In here player1 is boatsetup._1
-  var boatsetup: Map[ActorRef, Set[(Boat, BoatLocation)]] = Map()
-  //keep the state of the boards
-  var boardstates: Map[ActorRef, BoardState] = Map()
-
-
-  def numberofboatsetupsreceived:Int = {boatsetup.count((a: (ActorRef, Set[(Boat, BoatLocation)])) => true)}
-  //This is not functional programming at all, these functions are there only for the side effects...****************
-
-  def initGame(size:Int,player1:ActorRef,player2:ActorRef,boatset:Seq[Boat]):Any = {
-    players = (player1, player2)
-    boardsize = size
-    boatSet = boatset
-    players._1 ! PlaceBoats(boatset, boardsize)
-    sender() ! "Game started!"
-  }
-
-
-  def addboatsetup(placement:Set[(Boat,BoatLocation)]):Any = {
-    boatsetup += (currentPlayer -> placement)
-  }
-  def setupplayerfields():Any ={
-    boardstates += (players._1 -> initPlayerState(players._2))
-    boardstates += (players._2 -> initPlayerState(players._1))
-  }
-  def updateaftermove(x:Int,y:Int):Any = boardstates += (currentPlayer -> boardstates(currentPlayer).processShot((x, y)))
-
-  //This is not functional programming at all, these functions are there only for the side effects...****************
-
-
-  override def receive: Receive = {
-    case StartGame(size, player1, player2, boatset) => {
-      initGame(size, player1, player2, boatset)
-    }
-    case ThisIsBoatSetup(placement) if (sender() == currentPlayer) && (numberofboatsetupsreceived < 1) => {
-        addboatsetup(placement)
-        swapPlayers
-        currentPlayer ! PlaceBoats(boatSet, boardsize)
-    }
-    case ThisIsBoatSetup(placement) if (sender() == currentPlayer) && (numberofboatsetupsreceived < 2) => {
-        addboatsetup(placement)
-        setupplayerfields()
-        currentPlayer ! GetNextShot(boardsize, List())
-    }
-    case ThisIsNextMove(x, y) if sender() == currentPlayer => {
-      updateaftermove(x,y)
-      if (boardstates(currentPlayer).allShipsSunk) {
-        currentPlayer ! ("Player " + currentPlayer +  " has won!!") //this is for test only
-        log.info("Player " + currentPlayer +  " has won!!")
-      }
-      else {
-        swapPlayers
-        currentPlayer ! GetNextShot(boardsize, List())
-      }
-    }
-  }
-  */
-
-
 }
 
--- src/test/scala/battleship/gameActorSpec.scala
+++ src/test/scala/battleship/gameActorSpec.scala
@@ -5,9 +5,9 @@ package battleship
   */
 
 import akka.actor.{ActorRef, ActorSystem}
-import akka.testkit.{TestKit, TestProbe}
+import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import battleship.PlayerActor.{GetNextShot, PlaceBoats}
-import battleship.game.{BoardState, Boat, Game, LinearPlayer}
+import battleship.game._
 import battleship.GameActor.{GameNotStartedYet, _}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
@@ -29,6 +29,120 @@ class GameActorSpec(_system: ActorSystem)
       shutdown(system)
     }
   "gameActor" should {
+
+    "Game should send started message after being started and aska for setup from player1" in {
+      val testplayer1 = TestProbe()
+      val testplayer2 = TestProbe()
+      val testProbe = TestProbe()
+      val testActorgame = system.actorOf(props)
+      val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
+
+
+      testProbe.send(testActorgame, StartGame(8, testplayer1.ref, testplayer2.ref,Seq(Boat(5), Boat(4), Boat(3), Boat(3), Boat(2))))
+      testProbe.expectMsgPF(500 millis) {
+        case "Game started!" => {
+          println("gameactortest: game started")
+        }
+      }
+      testplayer1.expectMsgPF(500 millis) {
+        case PlaceBoats(boatset, size) => {
+          println("Player1 asked for setup")
+        }
+      }
+    }
+   "Game should send msg to ask for second boardstate after first was started" in {
+     val testplayer1 = TestProbe()
+     val testplayer2 = TestProbe()
+     val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
+
+     val actorRef = TestActorRef[GameActor]
+     val actor = actorRef.underlyingActor
+     actor.context.become(actor.gameinit1(1,testplayer1.ref,testplayer2.ref,Seq(Boat(1))))
+     testplayer1.send(actorRef, ThisIsBoatSetup(placement))
+     testplayer2.expectMsgPF(500 millis) {
+       case PlaceBoats(a,b) => {
+         println("gameactor sent message to actor 2 asking for boat placement")
+       }
+     }
+   }
+    "Game should ask player 2 for the first move after game is setup" in {
+      val testplayer1 = TestProbe()
+      val testplayer2 = TestProbe()
+      val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
+      val actorRef = TestActorRef[GameActor]
+      val actor = actorRef.underlyingActor
+
+      actor.context.become(actor.gameinit2(testplayer1.ref,1,testplayer1.ref,testplayer2.ref,placement))
+      testplayer2.send(actorRef, ThisIsBoatSetup(placement))
+      testplayer2.expectMsgPF(500 millis) {
+        case GetNextShot(size, history) => {
+          println("gameactor sent message to actor 2 asking for shot")
+        }
+      }
+      testplayer1.send(actorRef,WhatIsGameState)
+      testplayer1.expectMsgPF(500 millis) {
+        case a => println(a)
+      }
+    }
+    "Game should switch players and ask for the next move after a move is done" in {
+      val testplayer1 = TestProbe()
+      val testplayer2 = TestProbe()
+      val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
+      val actorRef = TestActorRef[GameActor]
+      val actor = actorRef.underlyingActor
+      val boardstate = BoardState(Set((Boat(1),BoatLocation(1,1,true))),List())
+      actor.context.become(actor.gamestarted(testplayer1.ref,8,testplayer1.ref,testplayer2.ref,testplayer2.ref,Map(testplayer1.ref -> boardstate,testplayer2.ref -> boardstate)))
+      testplayer2.send(actorRef,ThisIsNextMove(2,2))
+      testplayer1.expectMsgPF(500 millis) {
+        case GetNextShot(size, history) => succeed
+      }
+    }
+    "If the game receives the final sink, the game should end" in {
+      val testplayer1 = TestProbe()
+      val testplayer2 = TestProbe()
+      val placement = Set((Boat(1),game.BoatLocation(0,0,true)))
+      val actorRef = TestActorRef[GameActor]
+      val actor = actorRef.underlyingActor
+      val boardstate = BoardState(Set((Boat(1),BoatLocation(0,0,true))),List())
+      actor.context.become(actor.gamestarted(testplayer1.ref,1,testplayer1.ref,testplayer2.ref,testplayer2.ref,Map(testplayer1.ref -> boardstate,testplayer2.ref -> boardstate)))
+      testplayer2.send(actorRef,ThisIsNextMove(0,0))
+      testplayer2.expectMsgPF(500 millis) {
+        case a:String => if (a.startsWith("The game ended")) succeed
+      }
+
+    }
+    "If asked for gamestatus it should give it in every gamestate" in {
+       //Todo, and implicit in 'Play the games given various commands' 'unit' test
+    }
+    "Finish the game correctly with 1-tilegame" in {
+      val testProbe = TestProbe()
+      val testplayer1 = TestProbe()
+      val testplayer2 = TestProbe()
+      val testActorgame = system.actorOf(props)
+      val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
+      //(1,1 should be 0,0?????
+
+      testProbe.send(testActorgame, StartGame(1, testplayer1.ref, testplayer2.ref,Seq(Boat(1))))
+      testplayer1.expectMsgPF(500 millis) {
+        case _ => "1"
+      }
+      testplayer1.send(testActorgame, ThisIsBoatSetup(placement))
+      testplayer2.expectMsgPF(500 millis) {
+        case _ => "2"
+      }
+      testplayer2.send(testActorgame, ThisIsBoatSetup(placement))
+      testplayer2.expectMsgPF(500 millis) {
+        case _ => "3"
+      }
+      testplayer2.send(testActorgame, ThisIsNextMove(1,1))
+      testplayer2.expectMsgPF(500 millis) {
+        case string: String => {
+          println(string)
+          val stringcomp = "Player " + testplayer2.ref +  " has won!!"
+          string should be(stringcomp)
+        }
+      }
+    }
     "Play the games given various commands" in {
       val testProbe = TestProbe()
       val testplayer1 = TestProbe()
@@ -103,34 +217,7 @@ class GameActorSpec(_system: ActorSystem)
 
 
     }
-    "Finish the game correctly with 1-tilegame" in {
-      val testProbe = TestProbe()
-      val testplayer1 = TestProbe()
-      val testplayer2 = TestProbe()
-      val testActorgame = system.actorOf(props)
-      val placement = Set((Boat(1),game.BoatLocation(1,1,true)))
 
 
-      testProbe.send(testActorgame, StartGame(1, testplayer1.ref, testplayer2.ref,Seq(Boat(1))))
-      testplayer1.expectMsgPF(500 millis) {
-        case _ => //
-      }
-      testplayer1.send(testActorgame, ThisIsBoatSetup(placement))
-      testplayer2.expectMsgPF(500 millis) {
-        case _ => //
-      }
-      testplayer2.send(testActorgame, ThisIsBoatSetup(placement))
-      testplayer2.expectMsgPF(500 millis) {
-        case _ => //
-      }
-      testplayer2.send(testActorgame, ThisIsNextMove(1,1))
-      testplayer2.expectMsgPF(500 millis) {
-        case string: String => {
-          println(string)
-          val stringcomp = "Player " + testplayer2.ref +  " has won!!"
-          string should be(stringcomp)
-        }
-      }
-    }
   }
 }
